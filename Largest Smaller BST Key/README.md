2c2d8bd7350340ea33e38bec6fa053dbe2cc8153
Thomas Zhang <thomaszhang97@gmail.com>
2020-06-08 21:09:04 -0400

    largest smaller BST problem

---
+++ Largest Smaller BST Key/README.md
@@ -0,0 +1,20 @@
+# Problem:
+
+Given a root of a Binary Search Tree (BST) and a number num, implement an efficient function findLargestSmallerKey that finds the largest key in the tree that is smaller than num. If such a number doesn’t exist, return -1. Assume that all keys in the tree are nonnegative.
+
+Analyze the time and space complexities of your solution.
+
+# Example:
+For num = 17 and the binary search tree below:
+![]()
+
+
+Your function would return:
+
+14 since it’s the largest key in the tree that is still smaller than 17.
+
+Constraints:
+
+[time limit] 5000ms
+[input] Node rootNode
+[output] integer
--- Number of Paths/solution.js
+++ Number of Paths/solution.js
@@ -10,12 +10,12 @@ function numOfPathsToDest(n) {
 }
 
 function findPath(x, y) {
-    if (y > x || x < 0 || y < 0) return 0             // off the grid or crossed line
-    else if (x === 0 && y === 0) return 1             // valid path
-    else return findPath(x-1, y) + findPath(x, y-1)   
+  if (y > x || x < 0 || y < 0) return 0;
+  // off the grid or crossed line
+  else if (x === 0 && y === 0) return 1;
+  // valid path
+  else return findPath(x - 1, y) + findPath(x, y - 1);
 }
-
-
 
 // With memoization, we store 'memoize' or store values we have already calculated
 // Logic remains the same, but instead of returning, we store the result in our 2d grid (n x n)
@@ -23,19 +23,22 @@ function findPath(x, y) {
 // We build up the possible paths to each cell, eventually to calculate the 2 numbers we need (x, y-2) and (x-2,y)
 // EX) n = 5    we want numPaths to 4,4 which is = numPaths(3,4) + numPaths(4,3)
 function numOfPathsToDest(n) {
-    let memo = []
+  let memo = [];
 
-    for (let x = 0; x < n; x++) {
-        memo.push(new Array(n).fill(-1))
-    }
-    return findPaths(n-1, n-1, memo)
+  for (let x = 0; x < n; x++) {
+    memo.push(new Array(n).fill(-1));
+  }
+  return findPaths(n - 1, n - 1, memo);
 }
 
 function findPaths(x, y, memo) {
-    if (x < 0 || y < 0 || y > x) memo[x][y] = 0        // respect boundaries
-    else if (memo[x][y] !== -1) return memo[x][y]      // if not -1, we have already calculated this value
-    else if (x === 0 && y === 0) memo[x][y] = 1        // reach destination
-    else memo[x][y] = findPaths(x-1, y, memo) + findPaths(x, y-1, memo)  // -1 case: we need to calculate this value
+  if (x < 0 || y < 0 || y > x) memo[x][y] = 0;
+  // respect boundaries
+  else if (memo[x][y] !== -1) return memo[x][y];
+  // if not -1, we have already calculated this value
+  else if (x === 0 && y === 0) memo[x][y] = 1;
+  // reach destination
+  else memo[x][y] = findPaths(x - 1, y, memo) + findPaths(x, y - 1, memo); // -1 case: we need to calculate this value
 
-    return memo[x][y]
+  return memo[x][y];
 }
