26e51fbf1385c18fc972ff35e9910cc7f97625d1
Thomas Zhang <thomaszhang97@gmail.com>
2020-06-10 15:32:56 -0400

    few test cases

--- Array Index & Element Equality/solution.js
+++ Array Index & Element Equality/solution.js
@@ -4,22 +4,28 @@
 // The array indicies are strictly monotonically increasing sequences (sorted & ascending)
 
 function indexEqualsValueSearch(arr) {
-    let left = 0;
-    let right = arr.length - 1
-    
-    while (left <= right) {
-      let mid = Math.floor(left + (right - left)/2)
-      
-      if (mid === arr[mid]) {
-        return mid
-      } else if (mid > arr[mid]) {    // index too big, everything before must also be too big
-        left = mid + 1
-      } else {          // mid < arr[mid] 
-        right = mid - 1
-      }
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor(left + (right - left) / 2);
+
+    if (mid === arr[mid]) {
+      return mid;
+    } else if (mid > arr[mid]) {
+      // index too big, everything before must also be too big
+      left = mid + 1;
+    } else {
+      // mid < arr[mid]
+      right = mid - 1;
     }
-    return -1
   }
+  return -1;
+}
 
+// Test
+let arr = [-8, 0, 2, 5]; // arr[2] === 2
+let arr1 = [-1,0,3,6]    // -1, no index in given arr satisfies arr[i] === i
 
-  
+console.log('Correct output should be 2:', indexEqualsValueSearch(arr));
+console.log('Correct output should be -1:', indexEqualsValueSearch(arr1));
