2ac07f03d0aee4312b194c864778c6c6010f7c75
Thomas Zhang <thomaszhang97@gmail.com>
2020-06-11 23:56:57 -0400

    explanation

--- Array Quadruplet /solution.js
+++ Array Quadruplet /solution.js
@@ -1,3 +1,10 @@
+// A naive, brute force approach would be to consider every single possible quadruplet in the given array
+// This would require 4 nested loops and result in a time complexity of O(N^4)
+// A better approach we can take is to start by sorting the given array in ascending order, this'll ensure we return the first valid array
+// Our first 2 loops will (arr[i] + arr[j]). We can then subtract it from sum and check j+1 through end of the array for 2 numbers that add up to that remaining sum
+// We use a 2 pointer approach start and end. 
+// If arr[start] + arr[end] is greater than remaining sum, we 
+
 function findArrayQuadruplet(arr, s) {
   arr.sort((a, b) => a - b);
   let results = [];
@@ -24,7 +31,9 @@ function findArrayQuadruplet(arr, s) {
   return results;
 }
 
-
 // Test
-let input = [2, 7, 4, 0, 9, 5, 1, 3]    // [0, 4, 7, 9] 
-console.log('This output should equal [0, 4, 7 ,9]', findArrayQuadruplet(input, 20))
+let input = [2, 7, 4, 0, 9, 5, 1, 3]; // [0, 4, 7, 9]
+console.log(
+  'This output should equal [0, 4, 7 ,9]',
+  findArrayQuadruplet(input, 20)
+);
