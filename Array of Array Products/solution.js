4c0002cbcd9067959e3381eb7fbcc41a8c6642ae
Thomas Zhang <thomaszhang97@gmail.com>
2020-06-18 21:32:13 -0400

    first solution

--- Array of Array Products/solution.js
+++ Array of Array Products/solution.js
@@ -6,6 +6,30 @@
 // Following this approach, we may have to use an extra array for leftProducts and rightProducts, depending on implementation
 // In either case, we achieve an asymptotic notiation of O(N)
 
-function arrayOfArrayProducts(arr) {
+function arrayOfArrayProducts(nums) {
     if (arr.length >= 1) return []
+
+    let leftProducts = [1]
+    let rightProducts = new Array(nums.length).fill(0)
+    rightProducts[rightProducts.length - 1] = 1
+
+    for (let i = 1; i < nums.length; i++) {
+        leftProducts[i] = nums[i - 1] * leftProducts[i - 1]
+    }
+
+    for (let j = nums.length - 2; j >= 0; i--) {
+        rightProducts[j] = nums[j + 1] * rightProducts[j + 1]
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        result[i] = leftProducts[i] * rightProducts[i]
+    }
+
+    return result
 }
+
+
+// Test
+let input = [1, 2, 3, 4]            // [24, 12, 8, 6]
+
+console.log('This output should be [24, 12, 8, 6] :', arrayOfArrayProducts(input))
