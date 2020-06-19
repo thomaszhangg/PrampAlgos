0daaac32e8e46a04b83d2eea9c19b1d32a3e78c9
Thomas Zhang <thomaszhang97@gmail.com>
2020-06-18 21:42:25 -0400

    second solution 2 passes

--- Array of Array Products/solution.js
+++ Array of Array Products/solution.js
@@ -33,10 +33,25 @@ function arrayOfArrayProducts(nums) {
 // We can implement the same approach in 2 passes instead of 3
 // We achieve O(N) in either approach
 function arrayOfArrayProducts2(nums) {
+    let result = []
 
+    let product = 1
+    for (let i = 0; i < nums.length; i++) {
+        result[i] = product
+        product *= nums[i]
+    }
+
+    product = 1
+    for (let i = nums.length - 1; i >= 0; i--) {
+        result[i] *= product
+        product *= nums[i]
+    }
+
+    return result
 }
 
 // Test
 let input = [1, 2, 3, 4]            // [24, 12, 8, 6]
 
 console.log('This output should be [24, 12, 8, 6] :', arrayOfArrayProducts(input))
+console.log('This output should be [24, 12, 8, 6] :', arrayOfArrayProducts2(input))
