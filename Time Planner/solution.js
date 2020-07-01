02ce27ed15c54f99e9ff28d68ee666ea55028e4d
Thomas Zhang <thomaszhang97@gmail.com>
2020-06-30 23:51:29 -0400

    naive solution

--- Time Planner/solution.js
+++ Time Planner/solution.js
@@ -1,1 +1,3 @@
 // Naive Solution
+// We could loop through both input arrays and check the intersection of every pair slows to find an over of atleast dur seconds
+// This would yield a time complexity of O(N*M)
